d87eaded1106b7e97612cc90b670977fd42a8347
falicitt <falicity@qq.com>
2022-05-11 13:57:35 +1200

    add code for whack-a-mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,39 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let cells = document.getElementsByTagName('td')
+console.log(cells)
+
+let randomCell = cells[Math.floor(Math.random() * cells.length)]
+console.log(randomCell)
+
+function showMole() {
+  randomCell = cells[Math.floor(Math.random() * cells.length)]
+  const mole = document.createElement('img')
+  randomCell.appendChild(mole)
+  mole.src = 'mole.png'
+  mole.width = '75'
+  console.log(randomCell)
+  randomCell.addEventListener('click', whackMole)
+}
+
+function playAudio() {
+  let myAudio = document.getElementById('myAudio')
+  console.log(myAudio)
+  myAudio.play()
+}
+
+function whackMole() {
+  randomCell.innerHTML = ''
+  playAudio()
+  //randomCell = cells[Math.floor(Math.random() * cells.length)]
+  showMole()
+}
+
+showMole()
+randomCell.addEventListener('click', whackMole)
+//randomCell.addEventListener('click', showMole)
+console.log(randomCell)
